8a335639edb0366c8aa4b2b9ff7a68806b4fda74
jlake <jim@blueskylabs.com>
2024-11-17 06:43:22 -0800

    defines for error conditions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 const addon = require('../build/Release/node_system_extension.node');
 
-module.exports = addon;
+module.exports = {
+  ...addon,
+  OSSystemExtensionErrorUnknown: 1,
+  OSSystemExtensionErrorMissingEntitlement: 2,
+  OSSystemExtensionErrorUnsupportedParentBundleLocation: 3,
+  OSSystemExtensionErrorExtensionNotFound: 4,
+  OSSystemExtensionErrorExtensionMissingIdentifier: 5,
+  OSSystemExtensionErrorDuplicateExtensionIdentifer: 6,
+  OSSystemExtensionErrorUnknownExtensionCategory: 7,
+  OSSystemExtensionErrorCodeSignatureInvalid: 8,
+  OSSystemExtensionErrorValidationFailed: 9,
+  OSSystemExtensionErrorForbiddenBySystemPolicy: 10,
+  OSSystemExtensionErrorRequestCanceled: 11,
+  OSSystemExtensionErrorRequestSuperseded: 12,
+  OSSystemExtensionErrorAuthorizationRequired: 13,
+};
